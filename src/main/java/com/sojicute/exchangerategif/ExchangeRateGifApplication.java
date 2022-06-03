cb9fd851c00627cc501904d38bebb30cb2edf957
sojicute <sojicute137@gmail.com>
2022-06-03 11:09:19 +0400

    first commit

---
+++ src/main/java/com/sojicute/exchangerategif/ExchangeRateGifApplication.java
@@ -0,0 +1,13 @@
+package com.sojicute.exchangerategif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateGifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateGifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sojicute/exchangerategif/ExchangeRateGifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sojicute.exchangerategif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateGifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
